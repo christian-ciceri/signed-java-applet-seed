81559c28fb2ce701c33517ec38774edaff77ae4c
christian.ciceri <christian.ciceri@gmail.com>
2015-04-07 18:25:13 +0200

    added stupid javadoc

--- src/main/java/minimal/DummyClass.java
+++ src/main/java/minimal/DummyClass.java
@@ -4,6 +4,10 @@ package minimal;
  * Created by christian on 7/04/15.
  */
 public class DummyClass {
+
+    /**
+     * @see DummyClass
+     */
     public void hello()
     {
         System.out.println("hello world");
