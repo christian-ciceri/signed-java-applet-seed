0eafc079947932bf82847e9afb785139d7f87ffb
christian.ciceri <christian.ciceri@gmail.com>
2015-04-13 18:47:44 +0200

    example method as pure composition of instance methods

--- src/main/java/minimal/test.scala
+++ src/main/java/minimal/test.scala
@@ -11,3 +11,37 @@ class Generic[T  <: Updatable]
         obj.update();
     }
 }
+
+class UserDAO
+{
+
+}
+trait IUser
+{
+  def isValidated():Boolean;
+}
+class User
+{
+  def isValidated():Boolean=
+  {
+    true
+  }
+}
+object User
+{}
+class UserService(dao: UserDAO)
+{
+  var dao=new UserDAO();
+
+  def findById(userId: String): User=
+  {
+    val instance=new User()
+    instance
+  }
+  def isValidated(user: User): Boolean=
+  {
+     user.isValidated()
+  }
+
+  val getValidated=findById _  andThen ((_ : User).isValidated)
+}
